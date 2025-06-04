309b3da808edab86ebe9126c4cd2008325eaa0aa
SearidangPa <dangsyncpa@gmail.com>
2025-06-04 15:27:39 -0400

    sa

--- lua/terminal-multiplexer/init.lua
+++ lua/terminal-multiplexer/init.lua
@@ -126,14 +126,14 @@ function TerminalMultiplexer:toggle_float_terminal(terminal_name)
     current_float_term_state.chan = vim.bo.channel
   end
 
-  self_ref:_set_up_buffer_keybind(current_float_term_state)
+  self_ref:_set_up_buffer_keybind(current_float_term_state, terminal_name)
   self_ref.last_terminal_name = terminal_name
   return self_ref.all_terminals[terminal_name]
 end
 
 --- === Private functions ===
 
-function TerminalMultiplexer:_set_up_buffer_keybind(current_float_term_state)
+function TerminalMultiplexer:_set_up_buffer_keybind(current_float_term_state, terminal_name)
   local self_ref = self
 
   local map_opts = { noremap = true, silent = true, buffer = current_float_term_state.bufnr }
@@ -143,13 +143,19 @@ function TerminalMultiplexer:_set_up_buffer_keybind(current_float_term_state)
   vim.keymap.set('n', '>', next_term, map_opts)
   vim.keymap.set('n', '<', prev_term, map_opts)
 
-  vim.keymap.set('n', 'q', function()
+  local function hide_terminal()
     if vim.api.nvim_win_is_valid(current_float_term_state.footer_win) then
       vim.api.nvim_win_hide(current_float_term_state.footer_win)
     end
     if vim.api.nvim_win_is_valid(current_float_term_state.win) then
       vim.api.nvim_win_hide(current_float_term_state.win)
     end
+  end
+
+  vim.keymap.set('n', 'q', hide_terminal, map_opts)
+  vim.keymap.set('n', '<BS>', function()
+    hide_terminal()
+    self_ref:delete_terminal(terminal_name)
   end, map_opts)
 end
 
